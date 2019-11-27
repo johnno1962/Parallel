3461b8087154a3a618883a1999dd58c550beb263
John Holdsworth <github@johnholdsworth.com>
2019-11-27 09:21:49 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Parallel",
+    platforms: [.macOS("10.12")],
+    products: [
+        .library(name: "Parallel", targets: ["Parallel"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "Parallel", dependencies: [], path: "Sources/"),
+//        .testTarget(name: "ParallelTests", dependencies: ["Parallel"], path: "ParallelTests/"),
+    ]
+)
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Parallel
+
 Some primitives for concurrent processing
+
+This project defines three simple primitives intended for use in projects that wish to paralellize 
+the processing of parcels of work across multiple threads/processors.
+
+* UnsafeLock - Alamofire's simple wrapper for os_unfair_lock
+
+* Synchronized - a wrapper for entities which are shared across threads.
+
+* concurrentMap - a member of the map family which distributes work across processors.
